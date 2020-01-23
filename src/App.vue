8bf7112a7c45b16c218dbcb332a861c3e5dd5d0d
Aiden Scott <aidenscott12@googlemail.com>
2020-01-23 16:23:05 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <todo-list />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TodoList from "./components/TodoList.vue";
+export default Vue.extend({
+  components: {
+    TodoList
+  },
+  data: function() {
+    return {
+      msg: "hello" as string
+    };
+  }
+});
+</script>>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+        <li> Todo A </li> 
+        <li> Todo B </li> 
+        <li> Todo C </li> 
+    </ul> 
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+
+});
+</script>>
+
+<style>
+</style>
